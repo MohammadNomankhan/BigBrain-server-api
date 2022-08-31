ad4b0a47f04173f5b3573fb8573acada41564f5a
MohammadNomankhan <khannoman1702@gmail.com>
2022-08-31 12:21:56 +0530

    changed config of knex

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ const knex = require('knex')({
   client: 'pg',
   connection: {
     connectionString : process.env.DATABASE_URL,
-    ssl: true
+    ssl: {
+    	rejectUnauthorized: false
+  }
   }
 });
 
